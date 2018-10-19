c30293dae03fdaa86954d1241e4f207be9af7087
John Estes <john.estes@4s-llc.com>
2018-10-19 13:48:49 -0400

    cleared out some old commented code, expanded search-boxes, added a number to the highlight start marker

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,6 @@
-var apiUrl = 'https://efassembly.com:4432'
+// var apiUrl = 'https://efassembly.com:4432'
 // var apiUrl = 'http://18.218.170.246:4200'
-// var apiUrl = 'http://localhost:4200'
+var apiUrl = 'http://localhost:4200'
 
 var activeTab = 0
 var dataExpanded = 0
@@ -3762,7 +3762,7 @@ function toggleHamburgerMenu() {
     }
     // showAd()
     // TAB SWITCH HERE
-    // switchTab(2)
+    switchTab(2)
     // viewSearch(0)
     // goToFbo(2, 0);
     // openPopups(2)
